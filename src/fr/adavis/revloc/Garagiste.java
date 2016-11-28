bbd882c319dde7c644afbb2a629f14b683c4f4a8
alexis <alexis.abid@gmail.com>
2016-11-28 16:54:43 +0100

    #4

--- src/fr/adavis/revloc/Garagiste.java
+++ src/fr/adavis/revloc/Garagiste.java
@@ -11,4 +11,28 @@ public class Garagiste {
 		this.nom = nom ;
 		this.adresse = adresse ;
 	}
+
+
+
+	public String getNom() {
+		return nom;
+	}
+
+
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
 }
